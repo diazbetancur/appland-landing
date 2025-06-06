c257b4ea495c8b2a0be0afc6497959c57d79beac
John Alexander Diaz Betancur <49962109+diazbetancur@users.noreply.github.com>
2025-06-06 10:15:24 -0500

    add clients

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { ServiceComponent } from './components/service/service.component';
+import { HomeComponent } from './feature/pages/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'about',
     component: AboutComponent,
@@ -12,11 +16,11 @@ const routes: Routes = [
   {
     path: 'service',
     component: ServiceComponent,
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
