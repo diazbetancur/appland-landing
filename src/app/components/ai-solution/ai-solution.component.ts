cecf90c912559212a37942ce8842ca0d546f2551
John Alexander Diaz Betancur <diazbetancur@gmail.com>
2025-07-05 13:28:23 -0500

    update

--- src/app/components/ai-solution/ai-solution.component.ts
+++ src/app/components/ai-solution/ai-solution.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-ai-solution',
   templateUrl: './ai-solution.component.html',
   styleUrls: ['./ai-solution.component.scss'],
 })
-export class AiSolutionComponent implements OnInit {
+export class AiSolutionComponent implements OnInit, OnDestroy {
   public aiSolutions: any[] = [];
+  private sub!: Subscription;
 
   constructor(private readonly translate: TranslateService) {}
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   ngOnInit(): void {
-    this.translate.get('IAServices.services').subscribe((data: any[]) => {
-      this.aiSolutions = data;
-    });
+    this.sub = this.translate
+      .get('IAServices.services')
+      .subscribe((data: any[]) => {
+        this.aiSolutions = data;
+      });
   }
 
   onCardHover(index: number) {}
--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
   styleUrls: ['./service.component.scss'],
 })
-export class ServiceComponent implements OnInit {
+export class ServiceComponent implements OnInit, OnDestroy {
   services: any[] = [];
+  private sub!: Subscription;
 
-  constructor(private readonly translate: TranslateService) {}
+  constructor(private readonly trans: TranslateService) {}
 
   ngOnInit(): void {
-    this.translate.get('ourServiceDescription').subscribe((data: any[]) => {
-      this.services = data;
+    this.sub = this.trans.stream('ourServiceDescription').subscribe((res) => {
+      this.services = res;
     });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
